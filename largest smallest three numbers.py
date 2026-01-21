46fc059f1c1f58a408b12c3eb329463e6a3e8650
keyavyas <keya.h.vyas@gmail.com>
2026-01-21 16:39:57 +0530

    Add files via upload

---
+++ largest smallest three numbers.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number "))
+b=int(input("Enter a number "))
+c=int(input("Enter a number "))
+
+if a>b and a>c :
+    print("largest = a")
+
+elif b>a and b>c :
+    print("largest = b")
+else :
+    print("largest = c")
