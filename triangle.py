64b4751ee75d44067a79a53f734f794e0ca37187
keyavyas <keya.h.vyas@gmail.com>
2026-01-21 17:03:29 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a angle "))
+b=int(input("Enter a angle "))
+c=int(input("Enter a angle "))
+
+if a>0 and b>0 and c>0 and (a+b+c==180):
+    print("Valid Triangle")
+else:
+    print("Not a valid Triangle")
