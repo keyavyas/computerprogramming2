34c1dc9c0be1f685850a4d45cc0ab6e2f124c385
keyavyas <keya.h.vyas@gmail.com>
2026-01-21 17:21:21 +0530

    Add files via upload

---
+++ collienear.py
@@ -0,0 +1,13 @@
+x1=int(input("Enter a number "))
+y1=int(input("Enter a number "))
+
+x2=int(input("Enter a number "))
+y2=int(input("Enter a number "))
+
+x3=int(input("Enter a number "))
+y3=int(input("Enter a number "))
+
+if (x2-x1)*(y3-y1)==(y2-y1)*(x3-x1):
+    print("All three points lie on straight line")
+else:
+    print("All three points do not lie on straight line")
