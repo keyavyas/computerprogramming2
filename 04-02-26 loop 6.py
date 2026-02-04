0ef806aacd4c2f8fdd0068103fa659c9a147da47
keyavyas <keya.h.vyas@gmail.com>
2026-02-04 17:46:32 +0530

    Add files via upload

---
+++ 04-02-26 loop 6.py
@@ -0,0 +1,10 @@
+for hour in range(0,24):
+
+    if hour==0:
+        print("12 Midnight")
+    elif hour ==12:
+        print("12 Noon")
+    elif hour <12:
+        print(hour,"AM")
+    else:
+        print(hour -12,"PM")
