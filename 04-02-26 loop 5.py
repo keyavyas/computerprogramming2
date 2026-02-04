04c5af055291066998f676dd358d8f168ebb2f83
keyavyas <keya.h.vyas@gmail.com>
2026-02-04 17:44:08 +0530

    Add files via upload

---
+++ 04-02-26 loop 5.py
@@ -0,0 +1,7 @@
+print("Pythagorean Triplets (side <=30):")
+
+for a in range(1,31):
+    for b in range(a,31):
+        for c in range(b,31):
+            if a*a + b*b == c*c:
+                print(a,b,c)
